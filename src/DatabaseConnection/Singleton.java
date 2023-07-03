ee94f7f8a97057779be7030874850ca04c82db7d
ElianHuanca <huancacori@gmail.com>
2023-07-03 19:38:56 -0400

    Singleton added

---
+++ src/DatabaseConnection/Singleton.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DatabaseConnection;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Henrryrj
+ */
+public class Singleton {
+
+    private static Singleton singletonConexion;
+    private final PostgresConnection conexion;
+    public Connection pgAdmin;
+
+    public Singleton() {
+        this.conexion = new PostgresConnection();
+        this.pgAdmin = conexion.getConnection();
+    }
+
+    public static Singleton getInstancia() {
+        if (singletonConexion == null) {
+            singletonConexion = new Singleton();
+        }
+
+        return singletonConexion;
+    }
+}
